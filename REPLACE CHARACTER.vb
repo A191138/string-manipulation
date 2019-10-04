c77ca68d23c74f8c8d417508b6c5225c9395082b
A191138 <55289470+A191138@users.noreply.github.com>
2019-10-04 23:41:42 +0500

    Add files via upload

---
+++ REPLACE CHARACTER.vb
@@ -0,0 +1,35 @@
+﻿Module Module1
+    Sub Main()
+
+        Dim str1, str2 As String
+        Dim char1, char2, nextchar As Char
+        Dim count As Integer
+
+        str1 = ""
+        str2 = ""
+        char1 = ""
+        char2 = ""
+        nextchar = ""
+
+        Console.WriteLine("please enter a string")
+        str1 = Console.ReadLine
+
+        Console.WriteLine("please enter the character to be replaced")
+        char1 = Console.ReadLine
+
+        Console.WriteLine("please enter a character to replace with")
+        char2 = Console.ReadLine
+
+        For count = 1 To Len(str1)
+            nextchar = Mid(str1, count, 1)
+            If nextchar = char1 Then
+                str2 = str2 & char2
+            Else
+                str2 = str2 & nextchar
+            End If
+        Next
+        Console.WriteLine("final string is " & str2)
+        Console.ReadKey()
+
+    End Sub
+End Module
