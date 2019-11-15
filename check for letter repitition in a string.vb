2f3203adb09732a9536d3e512749a646f38fb177
A191138 <55289470+A191138@users.noreply.github.com>
2019-11-15 21:53:25 +0500

    Add files via upload
    program language is vb.net
    
    program takes input of a string and outputs the most repeated character from that string

---
+++ check for letter repitition in a string.vb
@@ -0,0 +1,34 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim str, input As String
+        Dim nextchar As Char
+        Dim maxcount, count, n As Integer
+        Dim maxchar As Char
+        Dim charlist As New ArrayList
+        Console.WriteLine("please enter a word")
+        input = Console.ReadLine
+        str = input.ToUpper()
+
+        With charlist
+            For Each c In "ABCDEFGHIJKLMNOPQRSTUVWXYZ".ToCharArray
+                .Add(c)
+            Next
+        End With
+
+        For n = 0 To 25
+            nextchar = (charlist.Item(n).ToString)
+            count = str.Split(nextchar).Length - 1
+            Console.WriteLine(nextchar & "=" & count)
+            If count > maxcount Then
+                maxcount = count
+                maxchar = nextchar
+            End If
+        Next
+
+        Console.WriteLine("most repeated character is " & maxchar & " and occurring " & maxcount & " times")
+        Console.ReadKey()
+
+    End Sub
+
+End Module
